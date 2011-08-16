075c3b5b7edb346c8f5d502c5cb35608434361c3
Rijk Stofberg <rijk.stofberg@gmail.com>
2011-08-16 16:59:11 +0200

    Remove marker interface and start using a little bit more elaborate template.

--- rhaptos/atompub/plone/interfaces.py
+++ rhaptos/atompub/plone/interfaces.py
@@ -3,7 +3,3 @@ from zope.interface import Interface
 class IAtomPubServiceAdapter(Interface):
     """ Marker interface for AtomPubService adapters.
     """
-
-class IAtomFeedProvider(Interface):
-    """ Marker interface for Atom feed objects.
-    """
