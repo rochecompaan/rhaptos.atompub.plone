5dd151a4133e79c7f3bdb415d5d2b6e46509b1e5
Izak Burger <isburger@gmail.com>
2011-09-16 13:14:20 +0200

    Add UnsupportedMediaType exception
    Its the only other one mentioned by the atom spec. The rest appear to be
    sword specific.

--- rhaptos/atompub/plone/exceptions.py
+++ rhaptos/atompub/plone/exceptions.py
@@ -1,3 +1,5 @@
 class PreconditionFailed(Exception):
     """ Raise this when we want a 412 response. """
-    pass
+
+class UnsupportedMediaType(Exception):
+    """ Raise this when you want a 415 response. """
